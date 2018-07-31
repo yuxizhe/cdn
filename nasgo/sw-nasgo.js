805d0483161901ff98ad904d7b39227e0c296af5
yuxz <yuxz@xueqiu.com>
2018-07-31 17:34:58 +0800

    nasgo

---
+++ nasgo/sw-nasgo.js
@@ -0,0 +1,32 @@
+importScripts('/sw-toolbox.js')
+
+self.addEventListener('install', event => {
+  //取消更新后的等待
+  self.skipWaiting();
+});
+
+toolbox.router.get(/(.png|.jpg)/, toolbox.cacheFirst, {
+  cache: {
+      name: "sw-nasgo-img"
+  }
+})
+
+toolbox.router.get(/http.*(.js|.css)/, toolbox.cacheFirst, {
+  cache: {
+      name: "sw-nasgo-jscss"
+  }
+})
+
+// 首页缓存
+toolbox.router.get('/', toolbox.networkFirst, {
+  cache: {
+      name: "html"
+  }
+})
+
+// 接口缓存
+toolbox.router.get(/list/, toolbox.networkFirst, {
+  cache: {
+      name: "json"
+  }
+})
